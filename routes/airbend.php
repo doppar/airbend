f677b886fecaba93900ff831da6efd3b5b477c7a
Arif Hoque <arif@sulaco-tec.com>
2025-12-01 18:58:50 +0600

    dynamic channel publishing added

--- routes/airbend.php
+++ routes/airbend.php
@@ -8,4 +8,5 @@ $router = app('route');
 // Load the Airbend Route
 // ======================================
 
-$router->post('broadcasting/auth', [BroadcastAuthController::class, 'authenticate']);
+$router->post('broadcasting/auth', [BroadcastAuthController::class, 'authenticate'])
+    ->middleware(config('airbend.authorize.middleware'));
